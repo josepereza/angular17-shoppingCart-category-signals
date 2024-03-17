c068c59ed9ed517c4daaf29097d9fc6598bf8ef8
josepereza <josepereza66@gmail.com>
2024-03-17 07:44:38 +0100

    first commit6

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,17 +7,26 @@ import { IProduct } from '../interfaces/iproduct';
   providedIn: 'root'
 })
 export class ApiService {
- public categoria=signal<string | null>('electronics')
+ public categoria=signal<string | null>('all')
  public productos=signal<IProduct[]>([]);
   public categories=toSignal<string[]>(this.http.get<string[]>('https://fakestoreapi.com/products/categories'));
 
  
- allProducts(){
-  this.http.get<IProduct[]>(`https://fakestoreapi.com/products/category/${this.categoria()}`)
-  .subscribe(data=>{
-    this.productos.set(data)
-  })
- }
+  allProducts(){
+
+    if (this.categoria()==='all'){
+      this.http.get<IProduct[]>(`https://fakestoreapi.com/products`)
+      .subscribe(data=>{
+        this.productos.set(data)
+      })
+    } else {
+       this.http.get<IProduct[]>(`https://fakestoreapi.com/products/category/${this.categoria()}`)
+    .subscribe(data=>{
+      this.productos.set(data)
+    })
+    }
+   
+   }
   categorias=computed(()=>{
   const cate= this.categories()?.map((data,index)=>{
   return  {data,id:index} 
